e9d3100c93f1151f66f989dc72f86a463f864bd1
Nicci Sheets <niccisheets@aol.com>
2018-06-20 14:25:18 -0400

    fifteenth position test fail

--- test_tdd_fizzbuxx.rb
+++ test_tdd_fizzbuxx.rb
@@ -23,9 +23,14 @@ class TestTddFizzBuzz < Minitest::Test
     	assert_equal("mined", fizzbuzz()[2])
     end
 
-	def test_assert_fifth_position_is_mines
+	def test_assert_fifth_position_is_minds
 		assert_equal("minds", fizzbuzz()[4])
 	end
+
+	def test_assert_fifteenth_position_is_mined_minds
+		assert_equal("mined minds", fizzbuzz()[14])
+	end
+	
 
 
 end
